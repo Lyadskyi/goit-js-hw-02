522b4c32d72943708d5416310588ed67eaaf2310
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2023-12-14 01:51:39 +0200

    Task-3.js it's done

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,9 @@
 function formatMessage(message, maxLength) {
-	
+	if (message.length <= maxLength) {
+		return message;
+	} else {
+		return maxLength.length + "...";
+	}
 }
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
@@ -8,16 +12,3 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-
-const message = "JavaScript is awesome";
-console.log(message.length); // 21
-
-
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-// 	if (quantity * pricePerDroid <= customerCredits) {
-// 		return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
-// 	} else {
-// 		return `Insufficient funds!`;
-// 	}
-// }
